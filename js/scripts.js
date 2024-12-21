f04ab8c2d3c37f03d4c253fc513ceb70968e8590
lahoainhan <nhan.lh91@arena-multimedia.edu.vn>
2024-12-22 01:01:41 +0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Lấy các phần tử DOM
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+// Toggle menu khi nhấn nút
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('open');
+});
+
+
+
+
+
+
+
+
